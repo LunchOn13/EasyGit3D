04b3628dc94425cc9952a43d35848765bdb5b15f
jeong-mo <68532545+jeong-mo@users.noreply.github.com>
2022-04-02 01:14:57 +0900

    New_json

--- KJM_Flie/Testing1/Testing1/Program.cs
+++ KJM_Flie/Testing1/Testing1/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace Testing1
 {
     class Program
     {
-        
         static void example() // input 없이 
         {
             string text = "ABC\nDEF\tGHI JKL";
@@ -15,8 +16,13 @@ namespace Testing1
             {
                 Console.WriteLine(item); // 나눈 토큰을 출력
             }
+            JObject sonSpec = new JObject(
+                new JProperty("score", 9),
+                new JProperty("name", "손흥민"),
+                new JProperty("number", 7)
+            );
+            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\test.json", sonSpec.ToString());
         }
-
         static void example(string input) // input이 있을시  
         {
             char[] sep = { '\n', '\t', ' ' }; // 똑같이 input을 나눈다
@@ -31,10 +37,11 @@ namespace Testing1
        
         static void Main(string[] args)
         {
-            string maintext = Console.ReadLine();
+            
+            //string maintext = Console.ReadLine(); // 터미널에서 입력 받을수 있다.
 
-            //example();
-            Console.WriteLine("###HelloWorld###");
+            example();
+            //Console.WriteLine("###HelloWorld###");
             //example(maintext);
             Console.WriteLine("HelloWorld!");
         }
