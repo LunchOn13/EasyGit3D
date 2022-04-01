103884f635975c1ffc68a2ac9be5a6182fd02601
jeong-mo <68532545+jeong-mo@users.noreply.github.com>
2022-04-02 00:08:08 +0900

    example_c#

---
+++ KJM_Flie/Testing1/Testing1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Testing1
+{
+    class Program
+    {
+        
+        static void example() // input 없이 
+        {
+            string text = "ABC\nDEF\tGHI JKL";
+            char[] sep = { '\n', '\t', ' ' };  // 이것들로 토큰을 나눈다
+
+            string[] result = text.Split(sep); 
+            foreach (var item in result)
+            {
+                Console.WriteLine(item); // 나눈 토큰을 출력
+            }
+        }
+
+        static void example(string input) // input이 있을시  
+        {
+            char[] sep = { '\n', '\t', ' ' }; // 똑같이 input을 나눈다
+
+            string[] result = input.Split(sep);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            string maintext = Console.ReadLine();
+
+            //example();
+            Console.WriteLine("###HelloWorld###");
+            //example(maintext);
+            Console.WriteLine("HelloWorld!");
+        }
+    }
+}
