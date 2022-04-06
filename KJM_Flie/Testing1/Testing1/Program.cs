3a2b3b0cad278411e518c325ceb30cf14e7268a5
jeong-mo <68532545+jeong-mo@users.noreply.github.com>
2022-04-06 17:51:44 +0900

    0406

--- KJM_Flie/Testing1/Testing1/Program.cs
+++ KJM_Flie/Testing1/Testing1/Program.cs
@@ -6,6 +6,7 @@ namespace Testing1
 {
     class Program
     {
+        
         static void example() // input 없이 
         {
             string text = "AuthorString\nDataString\tTitleString ExString";
@@ -26,29 +27,54 @@ namespace Testing1
             );
             File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\test1_4.json", sonSpec.ToString());
         }
-        static void example(string input) // input이 입력시  
-        {                       
+
+        // git log 하면 나오는 commi의 정보
+        static void Log_Information(string input) 
+        {
             string text = input;
-                                //줄 바꿈 삭제가 안됨...
-            char[] sep = {/*'\r', '\n', '\t',*/ ' '}; 
+            char[] sep = { '\r', '\n', '\t', ' ' };
             string[] result = text.Split(sep, 15); // input을 ' '로 15개로 나눈다
 
             JObject sonSpec = new JObject(          // json 파일의 내용들을 string문자로 만들기
-                  new JProperty("Commit?", result[0].Equals("commit")? "true": "false")
-                  ,new JProperty("Commit_address", result[1])
-                  ,new JProperty("Author", result[3])
-                  ,new JProperty("Date", result[8] +" " +result[9] +" " +result[10] +" " +result[12] )
-                  ,new JProperty("Time", result[11])
-                  ,new JProperty("Title", result[14])
+                  /*new JProperty("Commit?", result[0].Equals("commit") ? "true" : "false")
+                  , new JProperty("Commit_address", result[1])
+                  , */
+                  new JProperty("CommitMassage", result[14])
+                  , new JProperty("DateAndTime", result[8] + " " + result[9] + " " + result[10] + " " + result[12] +" "+ result[11])
+                  , new JProperty("Author", result[3])
+                  
               );
-            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\test2.json", sonSpec.ToString());
+            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Git_Log.json", sonSpec.ToString());
             // json의 내용이 담긴 내용을 실제 json파일로 생성
         }
+
+
+        static void Branch(string input)
+        {
+            string text = input;
+            char[] sep = { '\r', '\n', '\t', ' ' };
+            string[] result = text.Split(sep, 15); // input을 ' '로 15개로 나눈다
+
+            JObject sonSpec = new JObject(          // json 파일의 내용들을 string문자로 만들기
+                  new JProperty("Commit?", result[0].Equals("commit") ? "true" : "false")
+                  , new JProperty("Commit_address", result[1])
+                  , new JProperty("Author", result[3])
+                  , new JProperty("Date", result[8] + " " + result[9] + " " + result[10] + " " + result[12])
+                  , new JProperty("Time", result[11])
+                  , new JProperty("Title", result[14])
+              );
+            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Git_Log_0406.json", sonSpec.ToString());
+            // json의 내용이 담긴 내용을 실제 json파일로 생성
+        }
+
+
+
         static void Main(string[] args)
         {
+
             string text = Console.ReadLine();   // 문자 열을 입력
-            example(text);
-            Console.WriteLine("### Main The End ###");
+            Log_Information(text); // git log 전용
+
         }
     }
 }
