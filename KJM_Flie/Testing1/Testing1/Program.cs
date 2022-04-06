403539f1a85c29d8cd6dc432322dd7b42aa3b3e4
jeong-mo <68532545+jeong-mo@users.noreply.github.com>
2022-04-06 18:33:23 +0900

    branch_mathod_create

--- KJM_Flie/Testing1/Testing1/Program.cs
+++ KJM_Flie/Testing1/Testing1/Program.cs
@@ -36,9 +36,6 @@ namespace Testing1
             string[] result = text.Split(sep, 15); // input을 ' '로 15개로 나눈다
 
             JObject sonSpec = new JObject(          // json 파일의 내용들을 string문자로 만들기
-                  /*new JProperty("Commit?", result[0].Equals("commit") ? "true" : "false")
-                  , new JProperty("Commit_address", result[1])
-                  , */
                   new JProperty("CommitMassage", result[14])
                   , new JProperty("DateAndTime", result[8] + " " + result[9] + " " + result[10] + " " + result[12] +" "+ result[11])
                   , new JProperty("Author", result[3])
@@ -48,33 +45,57 @@ namespace Testing1
             // json의 내용이 담긴 내용을 실제 json파일로 생성
         }
 
-
         static void Branch(string input)
         {
-            string text = input;
             char[] sep = { '\r', '\n', '\t', ' ' };
-            string[] result = text.Split(sep, 15); // input을 ' '로 15개로 나눈다
+            string[] result = input.Split(sep);
 
-            JObject sonSpec = new JObject(          // json 파일의 내용들을 string문자로 만들기
-                  new JProperty("Commit?", result[0].Equals("commit") ? "true" : "false")
-                  , new JProperty("Commit_address", result[1])
-                  , new JProperty("Author", result[3])
-                  , new JProperty("Date", result[8] + " " + result[9] + " " + result[10] + " " + result[12])
-                  , new JProperty("Time", result[11])
-                  , new JProperty("Title", result[14])
-              );
-            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Git_Log_0406.json", sonSpec.ToString());
-            // json의 내용이 담긴 내용을 실제 json파일로 생성
+            int StarIndex = 0;
+            for(int i = 0; i < result.Length; i++)
+                if (result[i].Equals("*")) StarIndex = i;
+
+            JObject sonSpec = new JObject();
+
+            sonSpec.Add(new JProperty("Check", result[StarIndex + 1]));
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if(!result[i].Equals("*"))
+                    sonSpec.Add(new JProperty("Branchs"+i, result[i]));
+            }
+
+            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Git_Branch.json", sonSpec.ToString());
         }
 
 
 
         static void Main(string[] args)
         {
+            Console.WriteLine("지금은 test중이라서 임시로 이런 식으로 진행");
+            Console.WriteLine("log, branch 둘중에서 하나만 입력하셈");
 
-            string text = Console.ReadLine();   // 문자 열을 입력
-            Log_Information(text); // git log 전용
+            string input1 = Console.ReadLine();
+
+            if (input1.Equals("branch"))
+            {
+                Console.WriteLine("내용 입력");
+                string text = Console.ReadLine();   // 문자 열을 입력
+                // 몇개인지는 나중에 for문 돌리기
+                Branch(text); // git branch 전용
+            }
+            else if (input1.Equals("log"))
+            {
+                string text = Console.ReadLine();   // 문자 열을 입력
+                Log_Information(text); // git log 전용
+            }
+
+
+
+
 
         }
     }
 }
+
+//##########################################################################
+//                          무제점 한 라인 만 입력이 되서 엔터가 안먹힘.
