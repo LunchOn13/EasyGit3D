6f3384c600ff496c5bfe467836c19a4ae92a5687
jeong-mo <68532545+jeong-mo@users.noreply.github.com>
2022-04-29 15:38:02 +0900

    [],수정전

--- KJM_Flie/Testing1/Testing1/Program.cs
+++ KJM_Flie/Testing1/Testing1/Program.cs
@@ -3,9 +3,31 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text;
 
 namespace Testing1
 {
+    public class JsonFile
+    {
+        public string checkout { get; set; }
+        public List<Branche> Branches { get; set; }
+    
+    }
+    public class Branche
+    {
+        public string Title { get; set; }
+        public List<Commit_info> Commites { get; set; }
+    }
+    public class Commit_info
+    {
+        public string Message { get; set; }
+        public string author { get; set; }
+        public string date { get; set; }
+    }
+
     class Program
     {
         static string checking;
@@ -15,32 +37,40 @@ namespace Testing1
             for (int i = 0; i < 6; i++)
             {
                 string ins = Console.ReadLine();
-                if (0 != i)
-                    output += ins;
+                output += ins;
                 output += " ";
             }
             return output;
         }
-        static JObject Commit() // "git log" 명령의 output
+        static List<Commit_info> Commit(int count) // "git log" 명령의 output
         {
-            string text = Log_seting();
-            string[] result = text.Split(' '); // input을 ' '로 나눈다
-            JObject sonSpec = new JObject();
+            Console.WriteLine("commit 입력");
+            List<Commit_info> ListCommit = new List<Commit_info>();
 
-            if(result.Length == 21)
-            {
-                sonSpec.Add(new JProperty("CommitMassage", result[18]));
-                sonSpec.Add(new JProperty("DateAndTime", result[7] + " " + result[8] + " " + result[9] + " " + result[11] + " " + result[10]));
-                sonSpec.Add(new JProperty("Author", result[2]));
+            for (int k = 0; k < count; k++) {
+                string text = Log_seting();
+                string[] result = text.Split(' '); // input을 ' '로 나눈다
+                JObject sonSpec = new JObject();
+
+                // 일반 (commit 1929a17f0f1473eb781e84310e947539c9fe4016 (HEAD -> KJM, origin/KJM))  X
+                // commit 메시지가 뛰엄뛰엄 되있을수 있기떄문에 같이 묶이용
+                string commit_massage = "";
+                for (int i = 19; i < result.Length; i++)
+                    commit_massage += result[i];
+
+                ListCommit.Add(new Commit_info()
+                {
+                    Message = commit_massage
+                    , author = result[3]
+                    , date = result[8] + " " + result[9] + " " + result[10] + " " + result[12] + " " + result[11]
+                });
+                //
             }
-            // json 파일의 내용들을 string문자로 만들기  
-            //File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Commit.json", sonSpec.ToString());
-            return sonSpec;
+            //string strJson = JsonConvert.SerializeObject(ListCommit.ToArray(), Formatting.Indented);
+            return ListCommit;
         }
         static List<String> Repository(string input) // "git branch" 명령의 output
         {
-            JObject sonSpec = new JObject();
-
             char[] sep = { '\r', '\n', '\t', ' ' };
             string[] result = input.Split(sep);
 
@@ -53,83 +83,61 @@ namespace Testing1
                     Buser.Add(result[i]);
             }
 
-            //JObject member = new JObject();
-            //
-            //for (int i = 0; i < Buser.Count; i++)
-            //{
-            //    JObject mem = new JObject();
-            //    int x;
-            //    JObject str = new JObject();
-            //    Console.WriteLine(Buser[i]+"의 커밋수 입력");
-            //
-            //    x = int.Parse(Console.ReadLine());
-            //    for(int j = 0; j < x; j++)
-            //    {
-            //        //str.Add(Commit());
-            //    }
-            //    mem.Add(new JProperty(Buser[i]+"", str));
-            //    member.Add(mem);
-            //}
             checking = result[StarIndex + 1];
-            sonSpec.Add(new JProperty("Check", result[StarIndex + 1]));
-            sonSpec.Add(new JProperty("member", Buser));
-
-            //File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\Branch.json", sonSpec.ToString());
             return Buser;
         }
+       
+        
         static void Main(string[] args)
         {
             List<String> BranchList = new List<String>();
-            JObject sonSpec = new JObject();
-            JObject Cfile = new JObject();
-
+            List<Branche> Branche = new List<Branche>();
+            List<JsonFile> JsonFile = new List<JsonFile>();
+           //JObject sonSpec = new JObject();
+            
 
             Console.WriteLine("branch의 리스트를 한줄로 입력 하기");
             string text = Console.ReadLine();   // 문자 열을 입력 //* KJM main
             BranchList = Repository(text); // git branch 전용
 
-            for(int i = 0; i< BranchList.Count; i++)
+            for (int i = 0; i< BranchList.Count; i++)
             {
                 Console.WriteLine(BranchList[i] + "의 커밋 갯수 입력");
-                int x = int.Parse(Console.ReadLine());
+                int commitCount = int.Parse(Console.ReadLine());
 
-                JObject li = new JObject();
-                for (int j = 0; j < x; j++)
-                    li.Add(new JProperty(j +"", Commit()));
+                //Console.WriteLine(CommitOfBranch);
 
-                Cfile.Add(new JProperty(BranchList[i] + "", li));
+                Branche.Add(new Branche()
+                {
+                    Title = BranchList[i]
+                    ,Commites = Commit(commitCount)
+                });
             }
+            JsonFile.Add(new JsonFile()
+            {
+                Checkout = checking
+                ,Branches = Branche
+            });
 
-            sonSpec.Add(new JProperty("Cheke", checking));
-            sonSpec.Add(new JProperty("member", Cfile));
+            string strJsonmain = JsonConvert.SerializeObject(JsonFile.ToArray(), Formatting.Indented);
+
+
+
+            //sonSpec.Add(new JProperty("checkout", checking));
+            //sonSpec.Add(new JProperty("branches", strJsonmain));
 
             Console.WriteLine("파일명 입력");
             string fliename = Console.ReadLine();
-            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\"+fliename +".json", sonSpec.ToString());
+
+            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\" + fliename + ".json", strJsonmain);
         }
     }
 }
 
+//              commit 을 ' '을 나눴을때 문자열이 몇개인지 화긴용 
+//            for(int i = 0; i<result.Length; i++)
+//            {
+//                Console.WriteLine(i + " : " + result[i]);
+//            }
+
 //commit 개수 추출하는 명령어
-/*
-         static void example() // input 없이 
-        {
-            string text = "AuthorString\nDataString\tTitleString ExString";
-            char[] sep = { '\n', '\t', ' '};  // 이것들로 토큰을 나눈다
-
-            string[] result = text.Split(sep); 
-
-            foreach (var item in result)   // 나눈 토큰을 출력
-            {                                    //
-                Console.WriteLine(item); //
-            }                                   //
-
-            JObject sonSpec = new JObject(
-                new JProperty("Author", result[0]),
-                new JProperty("Data", result[1]),
-                new JProperty("Title", result[2]),
-                new JProperty("ExTest", result[3])
-            );
-            File.WriteAllText(@"C:\Users\82103\Desktop\Swimming_on_git\KJM_Flie\test1_4.json", sonSpec.ToString());
-        }
- */
---
+++ KJM_Flie/Testing1/make_Json.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+
+    static void Main(string[] args)
+    {
+        //List<String> BranchList = new List<String>();
+        //JObject sonSpec = new JObject();
+        //JObject Cfile = new JObject();
+
+
+
+
+        Console.WriteLine("branch의 리스트를 한줄로 입력 하기");
+        string text = Console.ReadLine();   // 문자 열을 입력 //* KJM main
+        Console.WriteLine(text);
+        //BranchList = Repository(text); // git branch 전용
+
+
+    }
+    }
